59a2821e7c3baca2b3fd5f937331875e9988e027
cda94581 <cda94581@gmail.com>
2021-01-11 14:32:41 -0600

    fix navigation

--- website/templates/navigation.js
+++ website/templates/navigation.js
@@ -1,35 +1,24 @@
 function writeNav(levels) {
+	var pathBack;
 	switch (levels) {
 		case 1:
-			document.write('\
-				<link rel="stylesheet" href="../../styles/navigation.css"> \
-				<script src="../../scripts/navigation.js"></script> \
-				<div class="navigation" id="navigation"> \
-					<a href="javascript:void(0)" class="closeNavigation" onclick="closeNavigation()">&#215;</a> \
-					<a href="../home/">Home</a> \
-					<a href="../about/">About</a> \
-					<a href="../blasting_to_bedrock/">Blasting to Bedrock</a> \
-					<a href="../downloads/">Downloads</a> \
-					<a href="../assets/">Assets</a> \
-					<a href="../feedback/">Feedback</a> \
-			');
+			pathBack = "../";
 			break;
 		case 2:
-			document.write('\
-				<link rel="stylesheet" href="../../../styles/navigation.css"> \
-				<script src="../../../scripts/navigation.js"></script> \
-				<div class="navigation" id="navigation"> \
-					<a href="javascript:void(0)" class="closeNavigation" onclick="closeNavigation()">&#215;</a> \
-					<a href="../../home/">Home</a> \
-					<a href="../../about/">About</a> \
-					<a href="../../blasting_to_bedrock/">Blasting to Bedrock</a> \
-					<a href="../../downloads/">Downloads</a> \
-					<a href="../../assets/">Assets</a> \
-					<a href="../../feedback/">Feedback</a> \
-			');
+			pathBack = "../../";
 			break;
 	}
 	document.write('\
+		<link rel="stylesheet" href="' + pathBack + '../styles/navigation.css"> \
+		<script src="' + pathBack + '../scripts/navigation.js"></script> \
+		<div class="navigation" id="navigation"> \
+			<a href="javascript:void(0)" class="closeNavigation" onclick="closeNavigation()">&#215;</a> \
+			<a href="' + pathBack + 'home/">Home</a> \
+			<a href="' + pathBack + 'about/">About</a> \
+			<a href="' + pathBack + 'blasting_to_bedrock/">Blasting to Bedrock</a> \
+			<a href="' + pathBack + 'downloads/">Downloads</a> \
+			<a href="' + pathBack + 'assets/">Assets</a> \
+			<a href="' + pathBack + 'feedback/">Feedback</a> \
 		</div> \
 		<a href="javascript:void(0)" onclick="openNavigation()" id="openNavigation">&#9776;</a> \
 	');
