5be9f249eb52b2299b5bbb42b6c38f06e8b8a1e0
cda94581 <cda94581@gmail.com>
2021-01-11 14:35:58 -0600

    fix nav style

--- website/templates/navigation.js
+++ website/templates/navigation.js
@@ -13,12 +13,12 @@ function writeNav(levels) {
 		<script src="' + pathBack + '../scripts/navigation.js"></script> \
 		<div class="navigation" id="navigation"> \
 			<a href="javascript:void(0)" class="closeNavigation" onclick="closeNavigation()">&#215;</a> \
-			<a href="' + pathBack + 'home/">Home</a> \
-			<a href="' + pathBack + 'about/">About</a> \
-			<a href="' + pathBack + 'blasting_to_bedrock/">Blasting to Bedrock</a> \
-			<a href="' + pathBack + 'downloads/">Downloads</a> \
-			<a href="' + pathBack + 'assets/">Assets</a> \
-			<a href="' + pathBack + 'feedback/">Feedback</a> \
+			<a href="' + pathBack + 'home">Home</a> \
+			<a href="' + pathBack + 'about">About</a> \
+			<a href="' + pathBack + 'blasting_to_bedrock">Blasting to Bedrock</a> \
+			<a href="' + pathBack + 'downloads">Downloads</a> \
+			<a href="' + pathBack + 'assets">Assets</a> \
+			<a href="' + pathBack + 'feedback">Feedback</a> \
 		</div> \
 		<a href="javascript:void(0)" onclick="openNavigation()" id="openNavigation">&#9776;</a> \
 	');
