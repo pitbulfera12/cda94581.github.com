25ef5eeded974a9c8b36889f69c5dc19d193138a
cda94581 <cda94581@gmail.com>
2021-01-11 14:05:44 -0600

    remove .html from all pages

--- website/templates/navigation.js
+++ website/templates/navigation.js
@@ -2,20 +2,6 @@ function writeNav(levels) {
 	switch (levels) {
 		case 1:
 			document.write('\
-				<link rel="stylesheet" href="../styles/navigation.css"> \
-				<script src="../scripts/navigation.js"></script> \
-				<div class="navigation" id="navigation"> \
-					<a href="javascript:void(0)" class="closeNavigation" onclick="closeNavigation()">&#215;</a> \
-					<a href="home/">Home</a> \
-					<a href="about/">About</a> \
-					<a href="blasting_to_bedrock/">Blasting to Bedrock</a> \
-					<a href="downloads/">Downloads</a> \
-					<a href="assets/">Assets</a> \
-					<a href="feedback/">Feedback</a> \
-			');
-			break;
-		case 2:
-			document.write('\
 				<link rel="stylesheet" href="../../styles/navigation.css"> \
 				<script src="../../scripts/navigation.js"></script> \
 				<div class="navigation" id="navigation"> \
@@ -26,6 +12,20 @@ function writeNav(levels) {
 					<a href="../downloads/">Downloads</a> \
 					<a href="../assets/">Assets</a> \
 					<a href="../feedback/">Feedback</a> \
+			');
+			break;
+		case 2:
+			document.write('\
+				<link rel="stylesheet" href="../../../styles/navigation.css"> \
+				<script src="../../../scripts/navigation.js"></script> \
+				<div class="navigation" id="navigation"> \
+					<a href="javascript:void(0)" class="closeNavigation" onclick="closeNavigation()">&#215;</a> \
+					<a href="../../home/">Home</a> \
+					<a href="../../about/">About</a> \
+					<a href="../../blasting_to_bedrock/">Blasting to Bedrock</a> \
+					<a href="../../downloads/">Downloads</a> \
+					<a href="../../assets/">Assets</a> \
+					<a href="../../feedback/">Feedback</a> \
 			');
 			break;
 	}
